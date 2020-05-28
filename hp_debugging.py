67c20287689a0c90612a03ac4540770d0bd52d01
Joris Nettelstroth <joris.nettelstroth@stw.de>
2020-05-28 17:04:44 +0200

    Put tests into one script with options

---
+++ hp_debugging.py
@@ -0,0 +1,74 @@
+"""Example script for testing the latest OMPython with AixLib 0.9.1.
+
+While the simulation of the CHP works just fine, the HeatPump example
+fails without any error message.
+After the simulate() command is called, the script simply never
+finishes and an omc.exe task keeps running seemingly indefinitely.
+
+python -m pip install -U https://github.com/OpenModelica/OMPython/archive/master.zip
+"""
+
+import os
+import sys
+from OMPython import OMCSessionZMQ
+
+
+def run_OpenModelica_CLI():
+    """Run simulations with calls to the OpenModelica command line."""
+    # Run argument parser
+    args = run_ArgParser()
+
+    # Create a subdirectory for the simulation
+    sim_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                           'sim_'+sys.platform  # separate windows and linux
+                           )
+
+    if os.path.exists(sim_dir) is False:
+        os.mkdir(sim_dir)
+
+    omc = OMCSessionZMQ()
+    base_path = omc.sendExpression('getInstallationDirectoryPath()')
+    file = os.path.join(base_path, r'lib/omlibrary/AixLib 0.9.1/package.mo')
+
+    if args.model == 'CHP':
+        cmds = [
+            'loadFile("{}")'.format(file),
+            'cd("{}")'.format(sim_dir),
+            # CHP example
+            'checkModel(AixLib.FastHVAC.Examples.HeatGenerators.CHP.CHP)',
+            'simulate(AixLib.FastHVAC.Examples.HeatGenerators.CHP.CHP)',
+            'plot(temperatureSensor_after.T)',
+          ]
+    elif args.model == 'heatpump':
+        cmds = [
+            'loadFile("{}")'.format(file),
+            'cd("{}")'.format(sim_dir),
+            # heatpump example
+            'checkModel(AixLib.FastHVAC.Examples.HeatGenerators.HeatPump.HeatPump)',
+            'simulate(AixLib.FastHVAC.Examples.HeatGenerators.HeatPump.HeatPump)',
+            'plot(innerCycle.QCon)',
+          ]
+
+    for cmd in cmds:
+        print('\n{}:'.format(cmd))
+        answer = omc.sendExpression(cmd)
+        print('{}\n'.format(answer))
+
+
+def run_ArgParser():
+    """Define and run the argument parser. Return the chosen file path."""
+    import argparse
+
+    description = 'Run Modelica with AixLib example models'
+    parser = argparse.ArgumentParser(description=description,
+                                     formatter_class=argparse.
+                                     ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('-m', '--model', dest='model', help='Model to use ('
+                        '"CHP" or "heatpump").', type=str, default='CHP')
+
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    run_OpenModelica_CLI()
